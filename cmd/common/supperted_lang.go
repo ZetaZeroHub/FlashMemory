eb4b46bc08b0eafab895e7194c33405130fd233b
zhouzhanghao <zhouzh13@asiainfo.com>
2025-05-27 09:52:03 +0800

    feat(cmd): 添加支持的语言列表- 新增 SupportedLanguages 列表，包含多种编程语言的文件扩展名 - 支持的语言包括：Go, Python, JavaScript, TypeScript, Java, C++, C, Ruby, PHP - 此列表用于限定脚本支持的编程语言范围

---
+++ cmd/common/supperted_lang.go
@@ -0,0 +1,15 @@
+package common
+
+var (
+	SupportedLanguages = []string{
+		".go",
+		".py",
+		".js", ".jsx",
+		".ts", ".tsx",
+		".java",
+		".cpp", ".cc", ".cc", ".cxx", ".c++", ".hpp", ".h",
+		".c",
+		".rb",
+		".php",
+	}
+)
